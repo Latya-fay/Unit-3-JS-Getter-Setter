d9ebd080ef0a17b85ab50d9481e8a7b2f2398a31
Latya-fay <ndanumulatya7@gmail.com>
2024-12-16 23:16:55 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius; // Use public property instead of private
+  }
+
+  // Getter for diameter
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  // Setter for diameter
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+  }
+
+  // Getter for circumference
+  get circumference() {
+    return Math.PI * this.diameter;
+  }
+
+  // Setter for circumference
+  set circumference(circumference) {
+    this.radius = circumference / (2 * Math.PI);
+  }
+
+  // Getter for area
+  get area() {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+
+  // Setter for area
+  set area(area) {
+    this.radius = Math.sqrt(area / Math.PI);
+  }
+}
